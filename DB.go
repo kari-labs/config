b5d08b1d116cbf0a61875ce16e5419a941fb09d0
Matt Conway <mattconway55@gmail.com>
2019-09-02 17:24:55 -0400

    first commit on config package

---
+++ DB.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+type DBConfig struct {
+	Host     string
+	Port     uint16
+	User     string
+	Password string
+	Database string
+}
+
+type Config struct {
+	DB DBConfig
+}
+
+// New() creates a new instance of a configuration
+func New() *Config {
+	return &Config{
+		DB: DBConfig{
+			Host:     getEnv("DB_HOST", "127.0.0.1"),
+			Port:     getEnvAsUInt16("DB_PORT", 3306),
+			User:     getEnv("DB_USER", "root"),
+			Password: getEnv("DB_PASSWORD", "root"),
+			Database: getEnv("DB_DATABASE", "none"),
+		},
+	}
+}
+
+func getEnv(key, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
+
+func getEnvAsUInt16(key string, defaultVal uint16) uint16 {
+	valStr := getEnv(key, "")
+	if value, err := strconv.Atoi(valStr); err != nil {
+		return uint16(value)
+	}
+
+	return defaultVal
+}
